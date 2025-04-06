2b99f9d9c164d29963239535f7502f54f674264f
Siya Bansal <siyabansal964@gmail.com>
2025-04-06 21:31:28 +0530

    updated commit

---
+++ src/pages/educator/Educator.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+import NavBar from "../../components/educator/NavBar";
+import SideBar from "../../components/educator/SideBar";
+import { assets } from "../../assets/assets";
+
+const Educator = () => {
+  return (
+    <div className="text-default min-h-screen bg-white flex flex-col">
+      <NavBar />
+
+      <div className="flex flex-1">
+        <SideBar />
+        <div className="flex-1 p-4 overflow-auto">
+          <Outlet />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Educator;
